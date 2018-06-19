4f63c6baaac320183c101f7ac0efc2a7397cf32c
mjmm13 <devnull@localhost>
2018-06-19 08:41:54 -0400

    Fixed imports alphabetically

--- colorbrewer/__init__.py
+++ colorbrewer/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from __future__ import division, absolute_import
+from __future__ import absolute_import, division
 __version__ = "0.1.2"
 from six.moves import map
 
--- test/test_colorbrewer.py
+++ test/test_colorbrewer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from __future__ import division, absolute_import
+from __future__ import absolute_import, division
 
 """test_colorbrewer.py: test colorbrewer module
 """
